045222152a7654d8a6c6c919e2ecbabce86a8da7
Сергей <1@php-cat.com>
2023-01-27 08:02:40 +0500

    tag+

---
+++ database/migrations/2023_01_27_024329_create_tags_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+
+            // id - number
+            // title - string - max length 50 - valid
+            $table->string('title', 50);
+            // slug - string (автоматический транслит title)
+            $table->string('slug', 60);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}

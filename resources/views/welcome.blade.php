b640d5654a48ac92b8d11c5a36d6b180fae066e8
Сергей <1@php-cat.com>
2023-02-01 11:29:07 +0500

    33

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-   
     @auth
         <app-component />
     @else
         <div class="container">
-            <p>проходите регистрацию (справа на верху) и будет доступ к разделам на vue3 .. списки добавить удалить, постраничная навигация</p>
-            <p>это штука написанная по тз ... есть 2 раздела, заходите покликайте
-                описание в readme > <A href="https://github.com/nyosru/2301-test-lara-vue2"
+            <p>проходите регистрацию (справа на верху) и будет доступ к разделам на vue3 .. списки добавить удалить,
+                постраничная навигация</p>
+            <p>это штука написанная по тз ... есть 2 раздела, заходите покликайте</p>
+            <p>
+                гит этого проекта > <A href="https://github.com/nyosru/2301-test-lara-vue2"
                     target="_blank">https://github.com/nyosru/2301-test-lara-vue2</A>
+                <br />
+                описание в readme
             </p>
         </div>
     @endauth
-
 @endsection

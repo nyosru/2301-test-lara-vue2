a433be5803369beb1285325c824972eda1c19826
Сергей <1@php-cat.com>
2023-02-01 11:35:34 +0500

    33

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <app-component />
     @else
         <div class="container">
+            <img src="https://php-cat.com/phpcat/ya7.jpg" style="float:left; max-width: 300px; padding-right:1rem;"/>
             <p>проходите регистрацию (справа на верху) и будет доступ к разделам на vue3 .. списки добавить удалить,
                 постраничная навигация</p>
             <p>это штука написанная по тз ... есть 2 раздела, заходите покликайте</p>
